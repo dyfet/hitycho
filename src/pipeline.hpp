c726b2c551573b864a7981c06bb5bf99e4ffcc3c
David Sugar <tychosoft@gmail.com>
2025-11-17 13:39:38 -0500

    Improve and simplify pipeline

--- src/pipeline.hpp
+++ src/pipeline.hpp
@@ -9,119 +9,17 @@
 #include <hpx/synchronization/condition_variable.hpp>
 #include <hpx/synchronization/mutex.hpp>
 
-#include <atomic>
-
 namespace hitycho::system {
 template <typename T, std::size_t S>
 class pipeline {
 public:
-    explicit operator bool() const noexcept { return !closed_; }
-    auto operator!() const noexcept { return closed_; }
-    auto capacity() const noexcept { return S; }
-    virtual ~pipeline() { close(); }
+    pipeline() = default;
+    pipeline(const pipeline&) = delete;
+    ~pipeline() { close(); }
+    auto operator=(const pipeline&) -> pipeline& = delete;
 
-    auto is_open() const noexcept {
-        return !closed_;
-    }
-
-    auto empty() const noexcept {
-        const guard_t lock(lock_);
-        return count_ == 0;
-    }
-
-    auto count() const noexcept {
-        const guard_t lock(lock_);
-        return count_;
-    }
-
-    void clear() {
-        const guard_t lock(lock_);
-        auto prior = count_;
-        while (count_ && drop_head(false))
-            ;
-        if (prior > count_ && !closed_)
-            input_.notify_one();
-    }
-
-    void close() {
-        if (!closed_.exchange(true)) {
-            output_.notify_all();
-            input_.notify_all();
-            hpx::this_thread::yield();
-            clear();
-        }
-    }
-
-    auto drop() {
-        const guard_t lock(lock_);
-        return drop_head(count_ == S);
-    }
-
-    auto drop_if() { // drop if full
-        const guard_t lock(lock_);
-        if (count_ == S) return drop_head(true);
-        return false;
-    }
-
-    auto push(T&& data) {
-        lock_t lock(lock_);
-        while (!closed_) {
-            if (count_ < S) {
-                data_[tail_] = std::move(data);
-                tail_ = (tail_ + 1) % S;
-                if (count_++ == 0) { // notify no longer empty
-                    output_.notify_one();
-                    this->notify(true);
-                }
-                return true;
-            }
-            full(lock);
-        }
-        return false;
-    }
-
-    auto push(const T& data) {
-        lock_t lock(lock_);
-        while (!closed_) {
-            if (count_ < S) {
-                data_[tail_] = data;
-                tail_ = (tail_ + 1) % S;
-                if (count_++ == 0) { // notify no longer empty
-                    output_.notify_one();
-                    this->notify(true);
-                }
-                return true;
-            }
-            full(lock);
-        }
-        return false;
-    }
-
-    auto pull(T& out) {
-        lock_t lock(lock_);
-        while (!closed_) {
-            if (count_ > 0) {
-                out = std::move(data_[head_]);
-                clear_item(data_[head_], false); // moved...
-                head_ = (head_ + 1) % S;
-                if (count_-- == S) // notify push when no longer full...
-                    input_.notify_one();
-                if (!count_) // notify clears when emptied
-                    this->notify(false);
-                return true;
-            }
-            wait(lock);
-        }
-        return false;
-    }
-
-    template <typename Func>
-    auto peek(Func func) const -> bool {
-        const guard_t lock(lock_);
-        if (!count_) return false;
-        func(data_[head_]);
-        return true;
-    }
+    explicit operator bool() const { return !empty(); }
+    auto operator!() const -> bool { return empty(); }
 
     auto operator<<(T&& data) -> pipeline& {
         if (!push(std::move(data)))
@@ -141,6 +39,106 @@ public:
         return *this;
     }
 
+    auto size() {
+        const guard_t lock(mutex_);
+        return count_;
+    }
+
+    auto empty() -> bool {
+        const guard_t lock(mutex_);
+        return count_ == 0;
+    }
+
+    auto full() -> bool {
+        const guard_t lock(mutex_);
+        return count_ == S;
+    }
+
+    auto push(T&& in) -> bool {
+        lock_t lock(mutex_);
+        if (!insert(lock)) return false;
+        if constexpr (std::is_pointer_v<T>) {
+            data_[tail_] = std::exchange(in, nullptr);
+        } else {
+            data_[tail_] = std::move(in);
+        }
+        tail_ = (tail_ - 1) % S;
+        count_++;
+        return true;
+    }
+
+    auto push(const T& in) -> bool {
+        lock_t lock(mutex_);
+        if (!insert(lock)) return false;
+        data_[tail_] = in;
+        tail_ = (tail_ - 1) % S;
+        count_++;
+        return true;
+    }
+
+    auto drop() {
+        const guard_t lock(mutex_);
+        if (!count_) return false;
+        if (count_ == S && wait_) push_.notify_one();
+        clear(data_[head_]);
+        head_ = (head_ + 1) % S;
+        count_--;
+        return true;
+    }
+
+    auto pop() {
+        const guard_t lock(mutex_);
+        if (!count_) return false;
+        if (count_ == S && wait_) push_.notify_one();
+        tail_ = (tail_ - 1) % S;
+        clear(data_[tail_]);
+        count_--;
+        return true;
+    }
+
+    auto pop(T& out) -> bool {
+        lock_t lock(mutex_);
+        if (!remove(lock)) return false;
+        tail_ = (tail_ - 1) % S;
+        if constexpr (std::is_pointer_v<T>) {
+            out = std::exchange(data_[tail_], nullptr);
+        } else {
+            out = std::move(data_[tail_]);
+        }
+        count_--;
+        return true;
+    }
+
+    auto pull(T& out) -> bool {
+        lock_t lock(mutex_);
+        if (!remove(lock)) return false;
+        if constexpr (std::is_pointer_v<T>) {
+            out = std::exchange(data_[head_], nullptr);
+        } else {
+            out = std::move(data_[head_]);
+        }
+        head_ = (head_ - 1) % S;
+        count_--;
+        return true;
+    }
+
+    void clear() {
+        const guard_t lock(mutex_);
+        if (count_ == S && wait_) push_.notify_all();
+        purge();
+    }
+
+    void close() {
+        lock_t lock(mutex_);
+        if (std::exchange(closed_, true)) return;
+        purge();
+        while (wait_) {
+            push_.notify_all();
+            pull_.notify_all();
+            exit_.wait(lock);
+        }
+    }
+
 protected:
     static_assert(std::is_pointer_v<T> || std::is_default_constructible_v<T>,
     "T must be a pointer or a non-deleted default constructor");
@@ -148,88 +146,51 @@ protected:
     using lock_t = std::unique_lock<hpx::mutex>;
     using guard_t = std::lock_guard<hpx::mutex>;
 
-    mutable hpx::mutex lock_;
-    hpx::condition_variable input_, output_;
+    mutable hpx::mutex mutex_;
+    hpx::condition_variable push_, pull_, exit_;
     T data_[S]{};
-    unsigned head_{0}, tail_{0}, count_{0};
-    std::atomic<bool> closed_{false};
+    unsigned head_{0}, tail_{0}, wait_{0}, count_{0};
+    bool closed_{false};
 
-    virtual void wait(lock_t& lock) {
-        output_.wait(lock, [&] { return closed_ || count_ > 0; });
-    }
-
-    virtual void full(lock_t& lock) {
-        input_.wait(lock, [&] { return closed_ || count_ < S; });
-    }
-
-    virtual void drop([[maybe_unused]] const T& obj) {}
-    virtual void notify([[maybe_unused]] bool pending) {}
-
-    void clear_item(T& data, bool destroy = true) {
+    void clear(T& item) {
         if constexpr (std::is_pointer_v<T>) {
-            if (destroy)
-                delete data;
-            data = nullptr;
+            delete item; // if already null, skipped
+            item = nullptr;
         } else {
-            data = std::move(T{});
+            item = T{};
         }
     }
 
-    auto drop_head(bool notify = true) {
-        if (!count_) return false;
-        clear_item(data_[head_], true);
-        head_ = (head_ + 1) % S;
-        count_--;
-        if (notify)
-            input_.notify_one();
+    auto insert(lock_t& lock) -> bool {
+        while (count_ == S) {
+            ++wait_;
+            push_.wait(lock);
+            --wait_;
+            if (!wait_ && closed_) exit_.notify_one();
+            if (closed_) return false;
+        }
+        if (!count_ && wait_) pull_.notify_one();
         return true;
     }
-};
 
-template <typename T, std::size_t S>
-class drop_pipeline : public pipeline<T, S> {
-public:
-    drop_pipeline() = default;
-
-private:
-    using lock_t = std::unique_lock<hpx::mutex>;
-
-    void full([[maybe_unused]] lock_t& lock) final {
-        this->drop(this->data_[this->head_]); // optional drop fast proc
-        this->drop_head(false);               // silent drop...
-    }
-};
-
-template <typename T, std::size_t S>
-class throw_pipeline : public pipeline<T, S> {
-public:
-    throw_pipeline() = default;
-
-private:
-    using lock_t = std::unique_lock<hpx::mutex>;
-
-    void full([[maybe_unused]] lock_t& lock) final {
-        throw hitycho::invalid("Pipeline ful");
-    }
-};
-
-template <typename T, std::size_t S>
-class notify_pipeline : public pipeline<T, S> {
-public:
-    notify_pipeline() = default;
-
-    operator int() const noexcept { return notify_.handle(); } // select / poll
-    auto handle() const noexcept { return notify_.handle(); }
-    auto wait(int timeout = 0) noexcept { return notify_.wait(timeout); }
-
-    void notify(bool pending) final {
-        if (pending)
-            notify_.signal();
-        else
-            notify_.clear();
+    auto remove(lock_t& lock) -> bool {
+        while (count_ == S) {
+            ++wait_;
+            pull_.wait(lock);
+            --wait_;
+            if (!wait_ && closed_) exit_.notify_one();
+            if (closed_) return false;
+        }
+        if (count_ == S && wait_) push_.notify_one();
+        return true;
     }
 
-private:
-    system::notify_t notify_;
+    void purge() {
+        while (count_ > 0) {
+            clear(data_[head_]);
+            head_ = (head_ + 1) % S;
+            count_--;
+        }
+    }
 };
 } // namespace hitycho::system
